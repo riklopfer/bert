dbba783ab640ad53a3991bdc496d65de3e6b3e22
Russell Klopfer <russell@klopfer.us>
2019-04-24 17:10:51 -0400

    adding word sense processor

--- run_classifier.py
+++ run_classifier.py
@@ -456,6 +456,34 @@ class SectionCodeProcessor(DataProcessor):
           labels.append(line)
       return labels
 
+class WordSenseProcessor(DataProcessor):
+  """Processor for the CoLA data set (GLUE version)."""
+
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+  def get_test_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+  def get_labels(self, data_dir=None):
+    """See base class."""
+    with open(os.path.join(data_dir, 'sense_vocab.txt'), 'rb') as ifp:
+      labels = []
+      for line in ifp:
+        line = line.strip()
+        if line:
+          labels.append(line)
+      return labels
+
 
   def _create_examples(self, lines, set_type):
     """Creates examples for the training and dev sets."""
