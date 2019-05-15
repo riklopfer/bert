b8b028bf0dcfda659238bbbd43a130937b6fd818
Russell Klopfer <russell@klopfer.us>
2019-05-15 11:28:45 -0400

    better eval file name

--- run_classifier_eval.py
+++ run_classifier_eval.py
@@ -300,7 +300,9 @@ def main(_):
     result["Average F1"] = '{:.3%}'.format(total_f1 / len(label_list))
 
     output_eval_file = os.path.join(FLAGS.output_dir,
-                                    "eval_results_{}.txt".format(epoch_n))
+                                    "eval_results_{:02d}-{}.txt".
+                                    format(epoch_n,
+                                           os.path.basename(init_checkpoint)))
     with tf.gfile.GFile(output_eval_file, "w") as writer:
       tf.logging.info("***** Eval results for epoch %d *****", epoch_n)
       writer.write("Epochs = {}\n\n".format(epoch_n))
